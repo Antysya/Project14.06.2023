cb576ce253648bcbc177aa3e51d0c78fb36b6dae
Ann Eroshina <antysya@mail.ru>
2023-06-15 19:19:49 +0300

    урок 14.06.2023

---
+++ public/index.php
@@ -0,0 +1,80 @@
+<?php //обязатьельно, текс до конструкции не применяют сейчас, но работать будет
+
+//переменные начинаются с $
+
+//echo "Hello world";
+
+$variable = "John";
+
+echo $variable;
+
+$n =10;
+
+echo PHP_EOL; //перевод на новую строку
+echo $n+10; 
+
+$greetings ='Hello';
+
+//concat
+echo "<br>";
+echo $greetings . $variable . $n;
+echo "<br>";
+echo '$greetings $variable $n';
+echo "<br>";
+echo "$greetings $variable $n";
+
+//array
+
+$array = array(
+	'key'=>'value',
+	'key1'=>'value1'
+);
+
+print_r($array);
+
+$array = array(
+        'value',
+        'value1'
+);
+
+print_r($array);
+
+echo "</pre>";
+
+//функции
+function func (int $n, int $l){
+	return $n + $l;
+}
+
+echo func(10,5);
+
+//echo func('qwerty',5); //ошибка нельзя строку
+
+foreach ($array as $key =>$value){  //с использованием ключа
+	echo "$key => $value <br>";
+}
+
+foreach ($array as $value){  //без ключа
+        echo "$value <br>";
+}
+
+//
+
+echo "<pre>";
+print_r($_GET);
+echo "</pre>";
+
+
+$form = "
+<form method='post'>
+<input type='text' name='name'>
+<input type='submit'>
+</form>
+";
+
+echo $form;
+
+
+echo "<pre>";
+print_r($_GET);
+echo "</pre>";
